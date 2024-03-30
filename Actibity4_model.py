5c1218fdd20cb11caac35b88d341edf74b332e51
RoushanRepo <m22aie243@iitj.ac.in>
2024-03-30 16:28:48 +0530

    Model behaving unexpectedly Demo

--- Actibity4_model.py
+++ Actibity4_model.py
@@ -19,7 +19,7 @@ model = LinearRegression()
 model.fit(X_train, y_train)
 
 # Make predictions
-y_pred = model.predict(X_test)
+y_pred = model.predict(X_test+0.8)
 
 # Calculate mean squared error
 mse = mean_squared_error(y_test, y_pred)
