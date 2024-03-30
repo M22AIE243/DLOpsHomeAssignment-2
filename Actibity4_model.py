848bfa87fe4a3a8a2e7d8870de45574174994dc2
RoushanRepo <m22aie243@iitj.ac.in>
2024-03-30 14:43:05 +0530

    Activity4

---
+++ Actibity4_model.py
@@ -0,0 +1,34 @@
+# Simple Linear Regression Model
+
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
+
+# Generate some linearly related data
+np.random.seed(0)
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Train the linear regression model
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions
+y_pred = model.predict(X_test)
+
+# Calculate mean squared error
+mse = mean_squared_error(y_test, y_pred)
+print("Mean Squared Error:", mse)
+
+# Plot the data and the linear regression line
+plt.scatter(X_test, y_test, color='blue')
+plt.plot(X_test, y_pred, color='red')
+plt.xlabel('X')
+plt.ylabel('y')
+plt.title('Linear Regression')
+plt.show()
